e2a5e0ee2ecfcf4045b62ac13fa0bf54eaf40373
rayreveles <79915954+rayreveles@users.noreply.github.com>
2021-10-09 01:36:37 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+/*Rayná Araújo Reveles - 0050013521*/
+
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
